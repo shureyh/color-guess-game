075d3b07048bdf9fffca5956ca38adc3bf80373c
shureyh <116615940+shureyh@users.noreply.github.com>
2022-11-11 10:15:48 +0300

    added color squares and click event listener

---
+++ script.js
@@ -0,0 +1,35 @@
+var colors = [
+  "rgb(255, 0, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 0, 255)",
+  "rgb(255, 0, 255)"
+]
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("color-display");
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+  //add initial color to squares
+  squares[i].style.backgroundColor = colors[i];
+
+  //add click event on squares
+  squares[i].addEventListener("click", clickedSquare);
+
+  function clickedSquare() {
+    //grab color of clicked square
+    var clickedColor = this.style.backgroundColor;
+    //compare color to pickedColor
+    if (clickedColor == pickedColor) {
+      alert("Correct");
+    }
+    else {
+      alert("wrong");
+    }
+
+  }
+}
+
